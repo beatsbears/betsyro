a27da5d855fdc4646c3f1e119acf79c42e2b4452
Scott C. MacCallum <scmaccal@users.noreply.github.com>
2015-09-12 20:27:27 -0400

    Update betsyroMain.java

--- betsyroMain.java
+++ betsyroMain.java
@@ -1,3 +1,28 @@
+// betsyroMain.java
+
+// Betsyro is a software solution for online civil discourse and civic 
+// engagement. It aims to empower those that are unable to be physically present
+// at a political venue or otherwise participate in the democratic process.
+
+// Copyright (C) 2015  Scott C. MacCallum
+// scm@linux.com
+
+// Copyright (C) 2015  Andrew Scott
+// a.clayton.scott@gmail.com
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+
 import org.jibble.pircbot.*;
 
 public class betsyroMain {
